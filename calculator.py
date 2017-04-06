4ce1bf6a7da994390492b11d4d6ce1f715a44c10
Hackbright Student <no-reply@hackbrightacademy.com>
2017-04-05 17:30:08 -0700

    Our first version before making big changes to function

--- calculator.py
+++ calculator.py
@@ -6,24 +6,10 @@ calculator program yourself in this file.
 
 from arithmetic import *
 
+calculator_on = True
 
 # Your code goes here
-
-# tokens = input_string.split(" ")   # => ["pow", "3", "5"]
-
-#while True is going to be here
-input_string = raw_input()
-
-tokens = input_string.split(" ")
-
-# Get the operator and the numbers from the user's input
-
-operator = tokens[0]
-num1 = float(tokens[1])
-num2 = float(tokens[2])
-
-
-def my_calculator(operator, num1, num2 = None):
+def my_calculator(operator, num1 = None, num2 = None):
     """This function performs a mathmatical operation
         chosen based on user input
 
@@ -37,3 +23,24 @@ def my_calculator(operator, num1, num2 = None):
     if operator == "+":
         print add(num1, num2)
 
+    elif operator == 'q':
+        calculator_on = False
+
+# tokens = input_string.split(" ")   # => ["pow", "3", "5"]
+
+input_string = ""
+
+#while True is going to be here
+while calculator_on:
+    input_string = raw_input()
+
+    tokens = input_string.split(" ")
+
+    # Get the operator and the numbers from the user's input
+
+    operator = tokens[0]
+    if len(tokens) > 1:
+        num1 = float(tokens[1])
+        num2 = float(tokens[2])
+
+    my_calculator(operator, num1, num2)
