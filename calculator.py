7f93eb3073981427a92e511a1f3938a29bfee30b
Hackbright Student <no-reply@hackbrightacademy.com>
2017-04-05 17:35:42 -0700

    New function works!

--- calculator.py
+++ calculator.py
@@ -6,10 +6,8 @@ calculator program yourself in this file.
 
 from arithmetic import *
 
-calculator_on = True
-
 # Your code goes here
-def my_calculator(operator, num1 = None, num2 = None):
+def my_calculator():
     """This function performs a mathmatical operation
         chosen based on user input
 
@@ -17,30 +15,28 @@ def my_calculator(operator, num1 = None, num2 = None):
         the math functions in arithmetic.py
     """
 
-    #for testing:
-    #print add(num1, num2)
+    calculator_on = True
 
-    if operator == "+":
-        print add(num1, num2)
+    while calculator_on:
 
-    elif operator == 'q':
-        calculator_on = False
+        input_string = raw_input()
+
+        tokens = input_string.split(" ")
+
+        operator = tokens[0]
+
+        if len(tokens) > 1:
+            num1 = float(tokens[1])
+            num2 = float(tokens[2])
+
+        # Here we start doing the math
+        if operator == "+":
+            print add(num1, num2)
+
+        elif operator == 'q':
+            calculator_on = False
+            
 
 # tokens = input_string.split(" ")   # => ["pow", "3", "5"]
 
-input_string = ""
-
-#while True is going to be here
-while calculator_on:
-    input_string = raw_input()
-
-    tokens = input_string.split(" ")
-
-    # Get the operator and the numbers from the user's input
-
-    operator = tokens[0]
-    if len(tokens) > 1:
-        num1 = float(tokens[1])
-        num2 = float(tokens[2])
-
-    my_calculator(operator, num1, num2)
+my_calculator()
